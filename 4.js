82f5410ffb06ca141f53a3d6a19d6783e667a268
kanz1t <110126494+kanz1t@users.noreply.github.com>
2025-08-04 13:42:31 +0400

    Update 4.js
    Edit decisions

--- 4.js
+++ 4.js
@@ -1,28 +1,74 @@
-function DeviceName(name){
-    this.devise='desktop';
-    this.name=name;
+function ElectricalAppliance(name, power) {
+        this.name = name;
+        this.power = power;
+        this.isPlugged = false;
 }
-DeviceName.prototype.getCondition = function (power, on, time){
-    let status = on;
-    if (on == 1) {
-        status='Вкл';
-    }else{
-        status='Выкл';
-    }
-    console.log(`${this.name} потребляет ${power*time} Вт за ${time} часа!!  `)
-}
-function DeviseColor(name, color) {
-    this.color=color;
-    this.name=name;
-}
-
-DeviseColor.prototype= new DeviceName();
-
-const lamp = new DeviseColor('Лампа', 'red');
-const computer = new DeviseColor('Компьютер', 'black');
-
-console.log(lamp);
-lamp.getCondition(40,1,2);
-
-console.log(computer);
-computer.getCondition(180,1,4);
+      
+      ElectricalAppliance.prototype.plugIn = function() {
+        this.isPlugged = true;
+        console.log(`${this.name} включен в розетку.`);
+      };
+      
+      ElectricalAppliance.prototype.unplug = function() {
+        this.isPlugged = false;
+        console.log(`${this.name} выключен из розетки.`);
+      };
+      
+      function DeskLamp(name, power, brightness) {
+        ElectricalAppliance.call(this, name, power);
+        this.brightness = brightness;
+      }
+      
+      DeskLamp.prototype = Object.create(ElectricalAppliance.prototype);
+      DeskLamp.prototype.constructor = DeskLamp; 
+      
+      DeskLamp.prototype.adjustBrightness = function(newBrightness) {
+        this.brightness = newBrightness;
+        console.log(`${this.name}: яркость установлена на ${this.brightness}%.`);
+      };
+      
+      function Computer(name, power, processor, ram) {
+        ElectricalAppliance.call(this, name, power);
+        this.processor = processor;
+        this.ram = ram;
+      }
+      
+      Computer.prototype = Object.create(ElectricalAppliance.prototype);
+      Computer.prototype.constructor = Computer;
+      
+      Computer.prototype.runProgram = function(programName) {
+        if (this.isPlugged) {
+          console.log(`${this.name}: запускаю программу ${programName}.`);
+        } else {
+          console.log(`${this.name}: не могу запустить ${programName}, компьютер выключен.`);
+        }
+      };
+      
+      const lamp = new DeskLamp("Настольная лампа", 60, 75);
+      const computer = new Computer("Компьютер", 300, "Intel Core i7", 16);
+      
+      lamp.plugIn();
+      computer.plugIn();
+      
+      console.log(lamp);
+      console.log(computer);
+      
+      lamp.adjustBrightness(50);
+      computer.runProgram("Visual Studio Code");
+      lamp.unplug();
+      
+      function calculateTotalPower(appliances) {
+        let totalPower = 0;
+        for (let i = 0; i < appliances.length; i++) {
+          if (appliances[i].isPlugged) {
+            totalPower += appliances[i].power;
+          }
+        }
+        return totalPower;
+      }
+      
+      const appliances = [lamp, computer];
+      
+      const totalPower = calculateTotalPower(appliances);
+      console.log(`Суммарная потребляемая мощность включенных приборов: ${totalPower} Вт.`);
+      
